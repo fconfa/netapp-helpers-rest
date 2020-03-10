60a5d7250f885c50c791b9296976712fb2b6c044
fconfa <rbits@protonmail.com>
2020-03-10 12:08:37 +0100

    Added comments

--- test.sh
+++ test.sh
@@ -1,38 +1,64 @@
 #!/bin/bash
+#===========================================================
+# Example workflow - Automating backup of NetApp snapshot
+#===========================================================
 
-# Common variables
+#-------------------
+# User variables
+#-------------------
+
+# NetApp IP address or hostname. Used for HTTPS connections.
 NA_IP=<REDACTED>
+
+# NetApp vserver username and password
 NA_USER=<REDACTED>
 NA_PASS=<REDACTED>
-VSERVER_NAME=studioppz
-VOLUME_NAME=studioppz_qlik_data
+
+# Name of the volume
+VOLUME_NAME=ppz_qlik_data
+
+# Name of the LUN to map
 LUN_NAME=data01
+
+# Name of NetApp initiator group for mapping the LUN to the host
 LUN_IG=ppz_backup
-SNAPSHOT_NAME=snap
+
+# Base name for the snapshot. The final name is built using this value and concatenatig the unix time value
+SNAPSHOT_BASENAME=snap
+
+# Snapshot name suffix after succesfull backup.
 BACKUP_SUFFIX=.backup
+
+
+#-------------------
+# System variables
+#-------------------
 
 PY_BIN=python3
 SCRIPT_ARGS="--na $NA_IP --user $NA_USER --pass $NA_PASS"
 CMD="${PY_BIN} na_helpers_rest.py ${SCRIPT_ARGS}"
 
+#-------------------
+# START
+#-------------------
+
 echo '-----------------------------------------------------'
 
 echo '1) Create snap for volume'
 $CMD --snaplist --vol $VOLUME_NAME
-$CMD --snapcreate --vol $VOLUME_NAME --base_name $SNAPSHOT_NAME
+$CMD --snapcreate --vol $VOLUME_NAME --base_name $SNAPSHOT_BASENAME
 $CMD --snaplist --vol $VOLUME_NAME
 
 echo '-----------------------------------------------------'
 
 echo '2) rotate snapshots'
-$CMD --snaplist --vol $VOLUME_NAME
 $CMD --snaprotate --vol $VOLUME_NAME --retention 5
 $CMD --snaplist --vol $VOLUME_NAME
 
 echo '-----------------------------------------------------'
 
 echo '3) create clone out of latest snapshot'
-last_snap=`$CMD --snaplist --vol $VOLUME_NAME|egrep ^${SNAPSHOT_NAME}|grep -v ${BACKUP_SUFFIX}|head -1|cut -d' ' -f1`
+last_snap=`$CMD --snaplist --vol $VOLUME_NAME|egrep ^${SNAPSHOT_BASENAME}|grep -v ${BACKUP_SUFFIX}|head -1|cut -d' ' -f1`
 $CMD --clonecreate --vol $VOLUME_NAME --snap $last_snap --clone ${VOLUME_NAME}_clone
 
 echo '-----------------------------------------------------'
@@ -58,7 +84,6 @@ $CMD --snaplist --vol $VOLUME_NAME
 echo '-----------------------------------------------------'
 
 echo '8) rotate snapshots'
-$CMD --snaplist --vol $VOLUME_NAME
 $CMD --snaprotate --vol $VOLUME_NAME --retention 5
 $CMD --snaplist --vol $VOLUME_NAME
 
@@ -68,3 +93,4 @@ echo '9) destroy clone volume'
 $CMD --clonedelete --vol ${VOLUME_NAME}_clone
 
 echo '-----------------------------------------------------'
+echo done.
